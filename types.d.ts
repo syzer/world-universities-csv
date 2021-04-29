3bcbe31be26470be4c11ff3bb13f14e2aa0c0fa6
Lukasz Gintowt <syzer3@gmail.com>
2021-04-29 14:22:17 +0200

    Fix TS types warnings

--- types.d.ts
+++ types.d.ts
@@ -1,4 +1,4 @@
-export function search(string): any[];
-export function find(string): any[];
-export function isUniversityEmail(string): boolean;
-export function findByEmail(string): Array<{ value: string, label: string }>;
+export function search(str: string): any[];
+export function find(str: string): any[];
+export function isUniversityEmail(str: string): boolean;
+export function findByEmail(str: string): Array<{ value: string, label: string }>;
